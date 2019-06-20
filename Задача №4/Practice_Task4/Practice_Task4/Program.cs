eca09c6460bc8fec81292e87e6d11b7d66049e43
GwynbleiddRU <46821681+GwynbleiddRU@users.noreply.github.com>
2019-06-20 15:44:42 +0300

    Задание 119(б)
    Вычислить бесконечную сумму с заданной точностью е (е > 0). Считать, что требуемая точность достигнута, если вычислена сумма нескольких первых слагаемых и очередное слагаемое оказалось по модулю меньше, чем е, — это и все последующие слагаемые можно уже не учитывать.
    Сумма 1/(i(i+1))

---
+++ Задача №4/Practice_Task4/Practice_Task4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static Func<int, double> f = (i) => 1d / (i * (i + 1));
+
+        static void Main(string[] args)
+        {
+            double eps;
+            bool ok = false;
+            do
+            {
+                Console.Write("Введите epsilon: ");
+                string buf = Console.ReadLine();
+                ok = double.TryParse(buf, out eps);
+                if (!ok || eps <= 0)
+                    Console.WriteLine("epsilon задается неотрицательным числом");
+            } while (!ok || eps <= 0);
+
+            double sum = 0d;
+            int i = 1;
+            for (; i < 3; i++)
+                sum += f(i);
+            while (true)
+            {
+                if (f(i) < eps) break;
+                sum += f(i);
+                i++;
+            }
+            Console.WriteLine("Сумма равна: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
