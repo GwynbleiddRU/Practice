e82dc7370320c1fdc2d52c62050f75fc09a030ae
GwynbleiddRU <46821681+GwynbleiddRU@users.noreply.github.com>
2019-06-20 21:18:06 +0300

    Задание 60(д)
    Постановка задачи: http://piccash.net/56765/976642/

---
+++ Задача №3/Practice_Task3/Practice_Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x");
+            var x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            var y = double.Parse(Console.ReadLine());
+            var u = x * x + y * y <= 1 && y >= x && y >= -x ? Math.Sqrt(Math.Abs(x * x - 1)) : x + y;
+            Console.WriteLine("u = " + u);
+            Console.ReadKey();
+        }
+    }
+}
