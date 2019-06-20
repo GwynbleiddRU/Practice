764bb5d45ec982af86cf99c42319fad4db39bfaf
GwynbleiddRU <46821681+GwynbleiddRU@users.noreply.github.com>
2019-06-20 15:58:28 +0300

    Задание 839
    Зафиксируем натуральное K и перестановку чисел 1, ... , K (ее можно задать с помощью последовательности натуральных чисел p1, ... , pK, в которую входит каждое из чисел 1, ... , K). При шифровке в исходном тексте к каждой из последовательных групп по K символов применяется зафиксированная перестановка. Пусть K = 4 и перестановка есть 3, 2, 4, 1. Тогда группа символов s1, s2, s3, s4 заменяется на s3, s2, s4, s1. Если в последней группе меньше четырех символов, то к ней добавляются пробелы. Пользуясь изложенным способом:
    a) зашифровать данный текст
    б) расшифровать данный текст

---
+++ Задача №11/Practice_Task11/Practice_Task11/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите К");
+            var k = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите перестановку, разделяя числа пробелом");
+            var p = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            Console.WriteLine("Введите текст");
+            var s = Console.ReadLine();
+            for (int i = 0; i < s.Length % k; i++)
+                s += " ";
+            var output = Encrypt(s, p);
+            Console.WriteLine("Зашифрованный текст:");
+            Console.WriteLine(output);
+            Console.WriteLine("Расшифрованный текст:");
+            Console.WriteLine(Decrypt(output, p));
+            Console.ReadKey();
+        }
+
+        static string Encrypt(string s, int[] p)
+        {
+            var result = new StringBuilder();
+            for (int i = 1; i < s.Length; i += p.Length)
+            {
+                foreach (var n in p)
+                {
+                    result.Append(s[i - 1 + n - 1]);
+                }
+            }
+            return result.ToString();
+        }
+
+        static string Decrypt(string s, int[] p)
+        {
+            var result = new char[s.Length];
+            for (int i = 1; i < s.Length; i += p.Length)
+            {
+                for (int j = 0; j < p.Length; j++)
+                {
+                    result[i - 1 + p[j] - 1] = s[i - 1 + j];
+                }
+            }
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                result[p[i] - 1] = s[i];
+            }
+            var answer = new StringBuilder();
+            foreach (var e in result)
+            {
+                answer.Append(e);
+            }
+            return answer.ToString();
+        }
+    }
+}
