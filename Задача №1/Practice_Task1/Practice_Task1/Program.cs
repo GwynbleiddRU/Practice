06196c0018e44421baadc2b874e48f3f32ceb392
GwynbleiddRU <46821681+GwynbleiddRU@users.noreply.github.com>
2019-06-20 15:28:13 +0300

    0290. База террористов
    Спецслужбы получили информацию о том, что в труднодоступной части Муравийской пустыни расположена хорошо замаскированная база террористов. В руки спецслужб попал и план этой базы, которая с большой высоты выглядит как группа скальных обломков, весьма часто встречающихся среди Муравийских песков. Президент отдал приказ уничтожить базу крылатыми ракетами. Ваша задача - по карте пустыни, полученной со спутника и плану базы определить количество возможных положений базы террористов. Помните, что террористы могли привезти на территорию базы камни!
    
    Входные данные:
    В первой cтроке файла INPUT.TXT записаны числа Nb и Mb (1 ≤ Nb, Mb ≤ 20). В следующих Nb строках записан план базы. Каждая из этих строк содержит по Mb символов "#" (ASCII 35 ) или "." (ASCII 46). Символ "#" обозначает фрагмент базы, а символ "." - песок. В следующей строке записаны числа Nd и Md (1 ≤ Nd, Md ≤ 100). И остаток файла содержит карту участка пустыни на котором, предположительно, находится база террористов - Nd строк по Md символов "#" или "." в каждой.
    
    Тестирование выполнено с помощью средств сайта acmp.ru, проверка входных данных не требуется.

---
+++ Задача №1/Practice_Task1/Practice_Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0290
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввод размеров карты с помощью LINQ
+            int[] inputSize = Console.ReadLine().Split(new char[] { ' ' }).Select(s => int.Parse(s)).ToArray();
+            //Создание двухмерного массива символов
+            char[][] plan = new char[inputSize[0]][];
+            //Ввод символов 
+            for (int i = 0; i < inputSize[0]; i++)
+            {
+                plan[i] = Console.ReadLine().ToCharArray();
+
+            }
+
+            //Ввод размеров территории
+            int[] inputSize2 = Console.ReadLine().Split(new char[] { ' ' }).Select(s => int.Parse(s)).ToArray();
+            //Создание двухмерного массива символов территории
+            char[][] map = new char[inputSize2[0]][];
+
+            //Ввод символов
+            for (int i = 0; i < inputSize2[0]; i++)
+            {
+                map[i] = Console.ReadLine().ToCharArray();
+
+            }
+
+            //Счетчик вероятных расположений
+            int counter = 0;
+
+            //есть ли совпадение
+            bool isMatch = true;
+            for (int i = 0; i < inputSize2[0] - inputSize[0] + 1; i++)
+            {
+                for (int j = 0; j < inputSize2[1] - inputSize[1] + 1; j++)
+                {
+                    isMatch = true;
+                    for (int k = 0; k < inputSize[0]; k++)
+                    {
+                        for (int l = 0; l < inputSize[1]; l++)
+                        {
+                            if (plan[k][l] == '#' && plan[k][l] != map[i + k][j + l])
+                            {
+                                //Если совпадения нет, то выходим из внутреннего цикла
+                                isMatch = false;
+                                break;
+                            }
+                        }
+                        //Если совпадения нет, выходим из внешнего цикла
+                        if (!isMatch)
+                        {
+                            break;
+                        }
+
+                    }
+                    //Если переменная сохранила значение true, прибавляем к счетчику 1
+                    if (isMatch)
+                        counter++;
+                }
+            }
+            //Вывод
+            Console.WriteLine(counter);
+        }
+    }
+}
